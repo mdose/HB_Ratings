eb8f2adc228fad47eb93d428a2507a5d27a2c61d
Megan <megan.e.dose@gmail.com>
2017-08-01 17:38:04 -0700

    Judgemental Eye Part 2/WIP Adding new Users/Completed register_form page

--- server.py
+++ server.py
@@ -3,9 +3,10 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -23,7 +24,27 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
     a = jsonify([1,3])
-    return a
+    return render_template("homepage.html")
+
+@app.route('/users')
+def user_list():
+    """Show list of users"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    """Getting User Info from form"""
+
+    return render_template("register_form.html")
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    """Storing User Info"""
+
+    return redirect('/')
+
 
 
 if __name__ == "__main__":
